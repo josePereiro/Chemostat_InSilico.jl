2a1771d90dc66aefbb029f4a55cd29588fb69f0f
josePereiro <jose.pereiro.lab@gmail.com>
2021-01-10 10:27:15 -0500

    New pol bounds

--- scripts/2_Run_Dinamic.jl
+++ scripts/2_Run_Dinamic.jl
@@ -28,38 +28,68 @@ end
 
 ## ---------------------------------------------------------
 # Tools
-PROG_FIG_DIR = joinpath(InLP.DYN_FIGURES_DIR, "progress")
-CACHE_DIR = InLP.DYN_CACHE_DIR
-dat_file(;sim_params...) = joinpath(CACHE_DIR, 
-    InLP.mysavename("dyn_dat", "jls"; sim_params...))
+PROG_FIG_DIR = joinpath(InCh.DYN_FIGURES_DIR, "progress")
+DATA_DIR = InCh.DYN_DATA_DIR
+DATA_FILE_PREFFIX = "dyn_dat"
+dat_file(;sim_params...) = joinpath(DATA_DIR, 
+    InLP.mysavename(DATA_FILE_PREFFIX, "jls"; sim_params...))
 fig_file(;sim_params...) = joinpath(PROG_FIG_DIR, 
     InLP.mysavename("fig", "png"; sim_params...))
 
-make_dirs() = mkpath.([PROG_FIG_DIR, CACHE_DIR])
+make_dirs() = mkpath.([PROG_FIG_DIR, DATA_DIR])
 
-function check_stst(ts; w = 1000, th = 0.05)
+function check_stst(ts; stst_window, stst_th)
     @views for sym in [:X_ts, :sl_ts, :sg_ts, :D_ts]
         s = getfield(ts, sym)
-        length(s) <= w && return false
-        vs = s[end - w:end]
+        length(s) <= stst_window && return false
+        vs = s[end - stst_window:end]
         m = abs(mean(vs))
         m = m == 0 ? 1.0 : m
         s = std(vs)
-        s/m > th && return false
+        s/m > stst_th && return false
     end
     return true
 end
 
 ## ---------------------------------------------------------
+# Plot progress coroutine
+let
+    mtimes = Dict()
+    @async while true
+
+        for file in readdir(DATA_DIR)
+
+            !startswith(file, DATA_FILE_PREFFIX) && continue
+            cfile = joinpath(DATA_DIR, file)
+
+            t = get!(mtimes, file, 0.0)
+            ct = mtime(cfile)
+
+            # save fig
+            if abs(t - ct) > 0
+                status, TS, M = deserialize(cfile)
+                p = InLP.plot_res(M, TS)
+                fname = replace(file, DATA_FILE_PREFFIX => "fig")
+                ffile = joinpath(PROG_FIG_DIR, fname)
+                savefig(p, ffile)
+                mtimes[file] = ct
+            end
+
+        end
+        sleep(rand(20:35))
+    end
+end
+
+## ---------------------------------------------------------
 # Simulation 
-DAT = UJL.DictTree() # To Store relevant information
+INDEX = UJL.DictTree() # To Store relevant information
 @time let
-    # setup
     
+    # setup
     make_dirs()
 
     # base model
-    M0 = InLP.SimModel(;
+    M0 = INDEX[:M0] = InLP.SimModel(;
             δvatp = 2, 
             δvg = 3, 
             niters = Int(5e4),
@@ -69,7 +99,7 @@ DAT = UJL.DictTree() # To Store relevant information
             Δt = 0.5,
         )
 
-    @info "Starting simulation" now()
+    @info "Starting simulation -t($(nthreads()))" now()
 
     # cache
     InLP.vgvatp_cache(M0)
@@ -80,88 +110,114 @@ DAT = UJL.DictTree() # To Store relevant information
     check_stst_frec = 1000
     savefig_frec = 1000
     savedat_frec = 1000
+    info_frec = 100
     push_frec = 10
-    death_th = DAT[:death_th] = 1e-2
+    death_th = INDEX[:death_th] = 1e-2
 
     # Params
-    ϵs = [0.01; 0.1:0.1:1.0]
-    Ds = [0.001:0.001:0.017;]
     # Vls = [0.0, 0.1]
-    Vls = [0.0]
+    Vls = INDEX[:Vls] = [0.0]
+    Ds = INDEX[:Ds]= [0.003:0.003:0.018;]
+    ϵs = INDEX[:ϵs] = [0.01, 0.1, 0.5]
     # τs = [0.0, 0.0022]
-    τs = [0.0] 
+    τs = INDEX[:τs] = [0.0] 
     
-    c = 1
-    N = prod(length.([ϵs, Ds, Vls, τs]))
+    c = 0 # progress counter
+    params = Iterators.product(Vls, Ds, ϵs, τs) |> collect
+    N = length(params)
     @info "Computing $(N) iterations"
-    cfiles = Set([])
 
-    for D in Ds, ϵ in ϵs, Vl in Vls, τ in τs
+    wlock = ReentrantLock()
+    @threads for (Vl, D, ϵ, τ) in params
 
-        # Prepare simulation model
-        M = deepcopy(M0)
-        M.D, M.ϵ, M.Vl, M.τ = D, ϵ, Vl, τ
-        TS = InLP.ResTS()
-        sim_params = (;M.D, M.ϵ, M.Δt, M.σ, M.τ, M.cg, M.Vl)
+        # This must identify the iteration
+        sim_params = (;D, ϵ, τ, Vl, M0.Δt, M0.σ, M0.cg)
 
-        @info "Doing $c/$N ..." D ϵ Vl τ now()s
+        # Say hello
+        tc = nothing # thread progress counter
+        lock(wlock) do
+            tc = c += 1
+            @info "Starting $tc/$N ..." D ϵ Vl τ now() threadid()
+            println()
+        end
+        
+        # Setup or load cache
+        cfile = dat_file(;sim_params...)
+        if isfile(cfile)
+            status, TS, M = deserialize(cfile)
+            tslen = length(TS.X_ts)
+            lock(wlock) do
+                @info "cache loaded $tc/$N ... " M.X M.sl M.sg tslen status now() threadid()
+                println()
+            end
+        else
+            M = deepcopy(M0)
+            M.D, M.ϵ, M.Vl, M.τ = D, ϵ, Vl, τ
+            TS = InLP.ResTS()
+            tslen = length(TS.X_ts)
+            status = :unfinished
+        end
+        
         
         function on_iter(it, _)
 
-            # check cache
-            cfile = dat_file(;sim_params...)
-            push!(cfiles, cfile)
-            cached = it == 1 && isfile(cfile)
-            cached && (TS, M = deserialize(cfile))
+            status == :finished && return true
+
+            # update tslen
+            tslen = length(TS.X_ts)
             
             # check steady state
             stst = rem(it, check_stst_frec) == 0 && 
-                check_stst(TS; w = stst_window, th = stst_th)
+                check_stst(TS; stst_window, stst_th)
             
             # check cells die
             death = M.X < death_th
 
             # finish
             finish = death || stst || it == M.niters
+            finish && (status = :finished)
 
             push_state = it % push_frec == 0 || finish
             push_state && push!(TS, M)        
 
-            # save fig
-            save_fig = it == 1 || rem(it, savefig_frec) == 0 || 
-                it == M.niters || finish || !cached
-            save_fig && savefig(InLP.plot_res(M, TS), fig_file(;it, sim_params...))
+            # # save fig
+            # save_fig = it == 1 || rem(it, savefig_frec) == 0 || 
+            #     it == M.niters || finish
+            # if save_fig
+            #     lock(wlock) do
+            #         savefig(InLP.plot_res(M, TS), fig_file(;tslen, sim_params...))
+            #     end
+            # end
 
             # save data
-            save_dat = rem(it, savedat_frec) == 0 || it == M.niters || 
-                finish || !cached
-            save_dat && serialize(cfile, (;TS, M))
+            save_dat = rem(it, savedat_frec) == 0 || 
+                it == M.niters || finish
+            serialize(cfile, (;status, TS, M))
+            touch(cfile) # plotting trigger
+                
+            # info and lock dat
+            show_info = rem(it, info_frec) == 0 || finish || save_dat
+            if show_info
+                lock(wlock) do
+                    @info "Doing $tc/$N ... " it M.X M.sl M.sg tslen status D ϵ Vl τ now() threadid()
+                    println()
+                end
+            end
 
             return finish
         end
-        InLP.run_simulation!(M; on_iter, verbose = true, force = false)
-
-        c += 1
+        InLP.run_simulation!(M; on_iter, verbose = false, force = false)
+        
+        lock(wlock) do
+            @info "Finished $tc/$N ... " tslen now() threadid()
+            INDEX[:DFILE, Vl, D, ϵ, τ] = cfile
+            println()
+            GC.gc()
+        end
     end
-
-    ## ----------------------------------------------------------------------------
-    # Collecting and Bundle    
-    for (i, cfile) in enumerate(cfiles)
-        TS, M = deserialize(cfile)
-        @info "Doing $i/$N ... " M.D M.ϵ cfile; println()
-        DAT[:TS, M.Vl, M.D, M.ϵ] = TS
-        DAT[:M, M.Vl, M.D, M.ϵ] = M
-
-        push!(get!(DAT, [], :Ds), M.D)
-        push!(get!(DAT, [], :Vls), M.Vl)
-        push!(get!(DAT, [], :ϵs), M.ϵ)
-    end
-    sort!(unique!(DAT[:Ds]))
-    sort!(unique!(DAT[:Vls]))
-    sort!(unique!(DAT[:ϵs]))
+ 
 end
-varinfo(Main, r"DAT")
 
 ## ----------------------------------------------------------------------------
 # SAVING
-UJL.save_data(InCh.DYN_DATA_BUNDLE_FILE, DAT)
+UJL.save_data(InCh.DYN_DATA_INDEX_FILE, INDEX)
